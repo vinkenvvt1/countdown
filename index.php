54d9557556783526b9c7c69ac4a6f8dfa493221c
vinkenvvt1 <106219464+vinkenvvt1@users.noreply.github.com>
2024-02-19 21:08:45 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -13,33 +13,60 @@ body {background-color:#1c1b22;}
 <body>
 <div class="container">
 <?php
-$date_str_hanjia = "2024-1-30";
-$date_str_shujia = "2024-7-5";
-$t_1 = strtotime($date_str_hanjia);
-$t_2 = strtotime($date_str_shujia);
+$date_str_winter_h = "2024-1-30";
+$date_str_summer_h = "2024-7-5";
+$day_winter_h = 25*24*60*60;
+$day_summer_h = 56*24*60*60;
+$t_1 = strtotime($date_str_winter_h);
+$t_2 = strtotime($date_str_summer_h);
 $t_0 = time();
-$t_3 = $t_1 - $t_0;
+$t_3 = $t_1 - $t_0; //离开始还有几天
 $t_4 = $t_2 - $t_0;
+$t_5 = $t_1 + $day_winter_h - $t_0; //离结束还有几天
+$t_6 = $t_2 + $day_summer_h - $t_0;
 if($t_3<0){$t_3=ceil((0-$t_3)/60/60/24/365)*60*60*24*365+$t_1-$t_0;}
 if($t_4<0){$t_4=ceil((0-$t_4)/60/60/24/365)*60*60*24*365+$t_2-$t_0;}
+if($t_5<0){$t_5=ceil((0-$t_5)/60/60/24/365)*60*60*24*365+$t_5;}
+if($t_6<0){$t_6=ceil((0-$t_6)/60/60/24/365)*60*60*24*365+$t_6;}
 $t_5_yue = intval($t_3/60/60/24/30);
 $t_5_ri = intval($t_3/60/60/24%30);
 $t_6_yue = intval($t_4/60/60/24/30);
 $t_6_ri = intval($t_4/60/60/24%30);
+$t_7_yue = intval($t_5/60/60/24/30);
+$t_7_ri = intval($t_5/60/60/24%30);
+$t_8_yue = intval($t_6/60/60/24/30);
+$t_8_ri = intval($t_6/60/60/24%30);
 ?>
-<div class="small_text">离</div>
-<div class="text">寒假</div>
-<div class="small_text">还有</div>
+<div class="small_text">
 <?php
-echo '<div class="small_text">'.$t_5_yue."月".$t_5_ri."天</div>";
+if ($t_5 < $day_winter_h) {
+echo '<div class="text">寒假</div>已开始';
+echo '<br>';
+echo "寒假结束还有".$t_7_yue."月".$t_7_ri."天";
+}
+else {
+echo '<div class="text">寒假</div>未开始';
+echo '<br>';
+echo '寒假开始还有'.$t_5_yue."月".$t_5_ri."天";
+}
 ?>
+</div>
 <br>
-<div class="small_text">离</div>
-<div class="text">暑假</div>
-<div class="small_text">还有</div>
+<div class="small_text">
 <?php
-echo '<div class="small_text">'.$t_6_yue."月".$t_6_ri."天</div>";
+if ($t_6 < $day_summer_h) {
+echo '<div class="text">暑假</div>已开始';
+echo '<br>';
+echo "暑假结束还有".$t_8_yue."月".$t_8_ri."天";
+}
+else {
+echo '<div class="text">暑假</div>未开始';
+echo '<br>';
+echo '暑假开始还有'.$t_6_yue."月".$t_6_ri."天";
+}
 ?>
+</div>
+
 </div>
 </body>
 </html>
